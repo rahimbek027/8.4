858234577485e0868816d3c26128435b925eb719
rahimbek027 <xabibullayevrahimbek5@gmail.com>
2024-11-22 09:24:39 +0500

    message

--- src/components/HomeProducts.tsx
+++ src/components/HomeProducts.tsx
@@ -1,50 +1,44 @@
-import { useQuery } from "@tanstack/react-query";
-import { Axios } from "../hook/useAxios";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query"
+import { Axios } from "../hook/useAxios"
+import { useEffect, useState } from "react"
 import HomeItem from "./HomeItem";
 
+
 interface CategoriesIdType {
-    categoriesId: string | number;
+    categoriesId:string | number
 }
 
 export interface ProductsType {
-    id: number;
-    img: string;
-    title: string;
-    typeId: number;
-    sizeId: number;
-    price: string;
-    saveCount: number;
+    id:number;
+    img:string;
+    title:string;
+    typeId:number;
+    sizeId:number;
+    price:string;
+    saveCount:number
 }
 
-const HomeProducts = ({ categoriesId }: CategoriesIdType) => {
-    const { data = [], isLoading } = useQuery({
-        queryKey: ["products", categoriesId],
-        queryFn: () =>
-            Axios().get(
-                categoriesId == 1 ? "/products" : `/products?categoriesId=${categoriesId}`
-            ).then((res) => res.data),
-        staleTime: 1000 * 60, // 1 daqiqa
-        enabled: !!categoriesId, // categoriesId mavjud bo'lganda so'rov yuboradi
-    });
+const HomeProducts = ({categoriesId}:CategoriesIdType) => {
 
-    const [products, setProducts] = useState<Array<ProductsType>>([]);
+
+    const {data = []} = useQuery({
+        queryKey:["producrts", categoriesId],
+        queryFn:() => Axios().get(`/products?categoriesId=${categoriesId == 1 ? "" : categoriesId}`).then(res => res.data)
+    })
+
+    const [products, setProducts] = useState<Array<ProductsType>>(data)
 
     useEffect(() => {
-        setProducts(data);
-    }, [data]);
-
-    if (isLoading) {
-        return <div>Yuklanmoqda...</div>;
-    }
+        setProducts(data)
+    },[data])
+    
 
     return (
-        <div className="flex flex-wrap gap-y-5 justify-between">
-            {products.map((item: ProductsType) => (
-                <HomeItem key={item.id} item={item} />
-            ))}
-        </div>
-    );
-};
+        <div className="flex flex-wrap  gap-[25px]">
 
-export default HomeProducts;
+            {products.map((item:ProductsType) => <HomeItem key={item.id} item={item} />)}
+        </div>
+    )
+}
+
+export default HomeProducts
